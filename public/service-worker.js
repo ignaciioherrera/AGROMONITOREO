2fcd0bb9ade86bc9ae93a20f95bd1c696b599451
ignaciioherrera <ignaciioherrera@gmail.com>
2026-03-22 22:26:20 -0300

    actualizar icono

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "agro-monitor-v1";
+const CACHE_NAME = "agro-monitor-v2";
 const STATIC_ASSETS = ["/", "/index.html", "/static/js/main.chunk.js", "/static/js/bundle.js"];
 
 self.addEventListener("install", (e) => {
