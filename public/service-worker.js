bf9448314b527a9b1bcfa82e88e6e60f8e110df0
ignaciioherrera <ignaciioherrera@gmail.com>
2026-03-27 20:35:23 -0300

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "agro-monitor-v4";
+const CACHE_NAME = "agro-monitor-v5";
 const STATIC_ASSETS = ["/", "/index.html", "/static/js/main.chunk.js", "/static/js/bundle.js"];
 
 self.addEventListener("install", (e) => {
